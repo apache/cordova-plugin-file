4233391da5dbf2f2418a1839f823ed79d5ffec50
Andrew Grieve <agrieve@chromium.org>
2013-07-10 09:31:32 -0400

    [CB-4038] - Remove o.a.c.api namespace

--- src/android/FileUtils.java
+++ src/android/FileUtils.java
@@ -25,9 +25,9 @@ import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
-import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.apache.cordova.DirectoryManager;
 import org.apache.cordova.FileHelper;
 import org.apache.cordova.file.EncodingException;
