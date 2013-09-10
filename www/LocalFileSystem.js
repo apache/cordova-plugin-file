d20b0ac58bc9354686c3f6c883fa063d13071cf0
Andrew Grieve <agrieve@chromium.org>
2013-09-10 13:43:52 -0400

    [CB-4771] Expose TEMPORARY and PERSISTENT constants on window.

--- www/LocalFileSystem.js
+++ www/LocalFileSystem.js
@@ -19,16 +19,5 @@
  *
 */
 
-var exec = require('cordova/exec');
-
-/**
- * Represents a local file system.
- */
-var LocalFileSystem = function() {
-
-};
-
-LocalFileSystem.TEMPORARY = 0; //temporary, with no guarantee of persistence
-LocalFileSystem.PERSISTENT = 1; //persistent
-
-module.exports = LocalFileSystem;
+exports.TEMPORARY = 0;
+exports.PERSISTENT = 1;
