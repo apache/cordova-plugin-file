c06d35b9bab4843363b31c264d41b3384b29423c
Jake Cobb <jcobb@us.fujitsu.com>
2014-06-03 11:45:08 -0700

    Fix mis-handling of filesystem reference in Entry.moveTo ('this' used in closure).

--- www/Entry.js
+++ www/Entry.js
@@ -145,7 +145,8 @@ Entry.prototype.copyTo = function(parent, newName, successCallback, errorCallbac
     var fail = errorCallback && function(code) {
         errorCallback(new FileError(code));
     };
-    var srcURL = this.toInternalURL(),
+    var filesystem = this.filesystem, 
+        srcURL = this.toInternalURL(),
         // entry name
         name = newName || this.name,
         // success callback
@@ -153,7 +154,7 @@ Entry.prototype.copyTo = function(parent, newName, successCallback, errorCallbac
             if (entry) {
                 if (successCallback) {
                     // create appropriate Entry object
-                    var fs = entry.filesystemName ? new FileSystem(entry.filesystemName, {name:"", fullPath:"/"}) : this.filesystem;
+                    var fs = entry.filesystemName ? new FileSystem(entry.filesystemName, {name:"", fullPath:"/"}) : filesystem;
                     var result = (entry.isDirectory) ? new (require('./DirectoryEntry'))(entry.name, entry.fullPath, fs, entry.nativeURL) : new (require('org.apache.cordova.file.FileEntry'))(entry.name, entry.fullPath, fs, entry.nativeURL);
                     successCallback(result);
                 }
