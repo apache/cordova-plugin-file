de403d0324d7c5d4e94c3d8a457d543d22871757
Steve Gill <stevengill97@gmail.com>
2015-05-19 01:20:44 -0700

    Updated requestFileSystem.js so it can be parsed by uglifyJS

--- www/requestFileSystem.js
+++ www/requestFileSystem.js
@@ -20,11 +20,14 @@
 */
 
 //For browser platform: not all browsers use this file.
-if (cordova.platformId === "browser" && navigator.userAgent.search(/Chrome/) > 0) {
-    var requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
-    module.exports = requestFileSystem;
-    return;
+function checkBrowser() {
+    if (cordova.platformId === "browser" && navigator.userAgent.search(/Chrome/) > 0) {
+        var requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+        module.exports = requestFileSystem;
+        return;
+    }
 }
+checkBrowser();
 
 var argscheck = require('cordova/argscheck'),
     FileError = require('./FileError'),
