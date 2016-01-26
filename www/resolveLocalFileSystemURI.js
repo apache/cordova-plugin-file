79956e4c3c8a90abe3bfa237ce59dd6d1e482311
sgrebnov <v-segreb@microsoft.com>
2016-01-26 13:11:01 +0300

    CB-10419 cordova-plugin-file 4.0.0 error with browserify workflow
    github close #157

--- www/resolveLocalFileSystemURI.js
+++ www/resolveLocalFileSystemURI.js
@@ -18,72 +18,73 @@
  * under the License.
  *
 */
-
-//For browser platform: not all browsers use overrided `resolveLocalFileSystemURL`.
-function checkBrowser() {
-    if (cordova.platformId === "browser" && require('./isChrome')()) {
-        module.exports.resolveLocalFileSystemURL = window.resolveLocalFileSystemURL || window.webkitResolveLocalFileSystemURL;
-        return true;
+(function() {
+    //For browser platform: not all browsers use overrided `resolveLocalFileSystemURL`.
+    function checkBrowser() {
+        if (cordova.platformId === "browser" && require('./isChrome')()) {
+            module.exports.resolveLocalFileSystemURL = window.resolveLocalFileSystemURL || window.webkitResolveLocalFileSystemURL;
+            return true;
+        }
+        return false;
     }
-    return false;
-}
-if (checkBrowser()) {
-    return;
-}
-
-var argscheck = require('cordova/argscheck'),
-    DirectoryEntry = require('./DirectoryEntry'),
-    FileEntry = require('./FileEntry'),
-    FileError = require('./FileError'),
-    exec = require('cordova/exec');
-var fileSystems = require('./fileSystems');
-
-/**
- * Look up file system Entry referred to by local URI.
- * @param {DOMString} uri  URI referring to a local file or directory
- * @param successCallback  invoked with Entry object corresponding to URI
- * @param errorCallback    invoked if error occurs retrieving file system entry
- */
-module.exports.resolveLocalFileSystemURL = module.exports.resolveLocalFileSystemURL || function(uri, successCallback, errorCallback) {
-    argscheck.checkArgs('sFF', 'resolveLocalFileSystemURI', arguments);
-    // error callback
-    var fail = function(error) {
-        errorCallback && errorCallback(new FileError(error));
-    };
-    // sanity check for 'not:valid:filename' or '/not:valid:filename'
-    // file.spec.12 window.resolveLocalFileSystemURI should error (ENCODING_ERR) when resolving invalid URI with leading /.
-    if(!uri || uri.split(":").length > 2) {
-        setTimeout( function() {
-            fail(FileError.ENCODING_ERR);
-        },0);
+    if (checkBrowser()) {
         return;
     }
-    // if successful, return either a file or directory entry
-    var success = function(entry) {
-        if (entry) {
-            if (successCallback) {
-                // create appropriate Entry object
-                var fsName = entry.filesystemName || (entry.filesystem && entry.filesystem.name) || (entry.filesystem == window.PERSISTENT ? 'persistent' : 'temporary');
-                fileSystems.getFs(fsName, function(fs) {
-                    // This should happen only on platforms that haven't implemented requestAllFileSystems (windows)
-                    if (!fs) {
-                        fs = new FileSystem(fsName, {name:"", fullPath:"/"});
-                    }
-                    var result = (entry.isDirectory) ? new DirectoryEntry(entry.name, entry.fullPath, fs, entry.nativeURL) : new FileEntry(entry.name, entry.fullPath, fs, entry.nativeURL);
-                    successCallback(result);
-                });
+
+    var argscheck = require('cordova/argscheck'),
+        DirectoryEntry = require('./DirectoryEntry'),
+        FileEntry = require('./FileEntry'),
+        FileError = require('./FileError'),
+        exec = require('cordova/exec');
+    var fileSystems = require('./fileSystems');
+
+    /**
+     * Look up file system Entry referred to by local URI.
+     * @param {DOMString} uri  URI referring to a local file or directory
+     * @param successCallback  invoked with Entry object corresponding to URI
+     * @param errorCallback    invoked if error occurs retrieving file system entry
+     */
+    module.exports.resolveLocalFileSystemURL = module.exports.resolveLocalFileSystemURL || function(uri, successCallback, errorCallback) {
+        argscheck.checkArgs('sFF', 'resolveLocalFileSystemURI', arguments);
+        // error callback
+        var fail = function(error) {
+            errorCallback && errorCallback(new FileError(error));
+        };
+        // sanity check for 'not:valid:filename' or '/not:valid:filename'
+        // file.spec.12 window.resolveLocalFileSystemURI should error (ENCODING_ERR) when resolving invalid URI with leading /.
+        if(!uri || uri.split(":").length > 2) {
+            setTimeout( function() {
+                fail(FileError.ENCODING_ERR);
+            },0);
+            return;
+        }
+        // if successful, return either a file or directory entry
+        var success = function(entry) {
+            if (entry) {
+                if (successCallback) {
+                    // create appropriate Entry object
+                    var fsName = entry.filesystemName || (entry.filesystem && entry.filesystem.name) || (entry.filesystem == window.PERSISTENT ? 'persistent' : 'temporary');
+                    fileSystems.getFs(fsName, function(fs) {
+                        // This should happen only on platforms that haven't implemented requestAllFileSystems (windows)
+                        if (!fs) {
+                            fs = new FileSystem(fsName, {name:"", fullPath:"/"});
+                        }
+                        var result = (entry.isDirectory) ? new DirectoryEntry(entry.name, entry.fullPath, fs, entry.nativeURL) : new FileEntry(entry.name, entry.fullPath, fs, entry.nativeURL);
+                        successCallback(result);
+                    });
+                }
             }
-        }
-        else {
-            // no Entry object returned
-            fail(FileError.NOT_FOUND_ERR);
-        }
+            else {
+                // no Entry object returned
+                fail(FileError.NOT_FOUND_ERR);
+            }
+        };
+
+        exec(success, fail, "File", "resolveLocalFileSystemURI", [uri]);
     };
 
-    exec(success, fail, "File", "resolveLocalFileSystemURI", [uri]);
-};
-
-module.exports.resolveLocalFileSystemURI = function() {
-    console.log("resolveLocalFileSystemURI is deprecated. Please call resolveLocalFileSystemURL instead.");
-    module.exports.resolveLocalFileSystemURL.apply(this, arguments);
-};
+    module.exports.resolveLocalFileSystemURI = function() {
+        console.log("resolveLocalFileSystemURI is deprecated. Please call resolveLocalFileSystemURL instead.");
+        module.exports.resolveLocalFileSystemURL.apply(this, arguments);
+    };
+})();
