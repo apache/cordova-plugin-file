e666ba32e377f4db9058e716c6be3bf96c39ca2b
Simon MacDonald <simon.macdonald@gmail.com>
2016-04-05 12:14:03 -0400

    Replace PermissionHelper.java with cordova-plugin-compat

--- src/android/FileUtils.java
+++ src/android/FileUtils.java
@@ -32,6 +32,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PermissionHelper;
 import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
