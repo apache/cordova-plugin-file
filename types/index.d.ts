3478bcdde20ece8b67fdc1c6ed296a2e3543666e
avdoninv <avdoninv@iowa.uiowa.edu>
2018-03-08 11:25:00 -0600

    CB-13960: fix FileWriter.write argument type definition for typescript

--- types/index.d.ts
+++ types/index.d.ts
@@ -294,9 +294,9 @@ interface FileWriter extends FileSaver {
     length: number;
     /**
      * Write the supplied data to the file at position.
-     * @param {Blob} data The blob to write.
+     * @param {Blob|string} data The blob to write.
      */
-    write(data: Blob): void;
+    write(data: Blob|string): void;
     /**
      * The file position at which the next write will occur.
      * @param offset If nonnegative, an absolute byte offset into the file.
