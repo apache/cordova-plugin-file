524dba2fd41d0f51c88495da99d591115e307521
Rohit <jammer99@outlook.com>
2020-04-14 16:43:12 +0530

    Update DirectoryReader.js

--- www/DirectoryReader.js
+++ www/DirectoryReader.js
@@ -62,7 +62,7 @@ DirectoryReader.prototype.readEntries = function (successCallback, errorCallback
             entry.isFile = result[i].isFile;
             entry.name = result[i].name;
             entry.fullPath = result[i].fullPath;
-            entry.filesystem = new (require('./FileSystem'))(result[i].filesystem.name || result[i].filesystemName);
+            entry.filesystem = new (require('./FileSystem'))(fs);
             entry.nativeURL = result[i].nativeURL;
             retVal.push(entry);
         }
